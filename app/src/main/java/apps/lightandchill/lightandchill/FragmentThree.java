5cef1aafad49c7bb8db617319fecef29e410ae63
Sideness <julien.stefani.fr@gmail.com>
2016-02-26 16:43:39 +0100

    Début du code de sauvegarde de l'adresse IP

--- app/src/main/java/apps/lightandchill/lightandchill/FragmentThree.java
+++ app/src/main/java/apps/lightandchill/lightandchill/FragmentThree.java
@@ -1,12 +1,15 @@
 package apps.lightandchill.lightandchill;
 
 import android.content.Context;
-import android.net.Uri;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 
 /**
@@ -31,6 +34,34 @@ public class FragmentThree extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_fragment_three, container, false);
+        View view = inflater.inflate(R.layout.fragment_three, container, false);
+
+        final EditText etIP = (EditText)view.findViewById(R.id.etIP);
+        Button btPreferences = (Button)view.findViewById(R.id.btPreferences);
+        Button btShow = (Button)view.findViewById(R.id.btShow);
+        final TextView tvIP = (TextView)view.findViewById(R.id.tvResult);
+
+        btPreferences.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SharedPreferences sharedPref = getActivity().getPreferences(Context.MODE_PRIVATE);
+                SharedPreferences.Editor editor = sharedPref.edit();
+                editor.putString(getString(R.string.PrefIP), etIP.getText().toString());
+                editor.commit();
+                tvIP.setText(etIP.getText());
+            }
+        });
+
+        btShow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SharedPreferences sharedPref = getActivity().getPreferences(Context.MODE_PRIVATE);
+                String defaultValue = getResources().getString(R.string.IPDefault);
+                String state = sharedPref.getString(getString(R.string.PrefIP), defaultValue);
+                tvIP.setText(state);
+            }
+        });
+
+        return view;
     }
 }
